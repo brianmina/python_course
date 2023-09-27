516778aa085ec72cc12137fa5424c78319e47857
brianmina <brian.mina17@gmail.com>
2023-09-27 14:46:25 -0400

    starting chapter 6

--- chapter_5/toppings.py
+++ chapter_5/toppings.py
@@ -1,3 +1,3 @@
-requested_toppings = "mushrooms"
+requested_toppings = "mushrooms" 
 if requested_toppings != "anchovies":
     print("Hold anchiovies! ")
---
+++ chapter_6_dictionaries/alien.py
@@ -0,0 +1,22 @@
+
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
+print(alien_0['points'])
+
+
+print(alien_0)
+
+
+# add key value pairs to the dictionary.
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+
+alien_1 = {}
+
+alien_1["color"] = "green"
+alien_1["points"] = 5
+
+print(alien_1 )
