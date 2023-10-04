decf8d03d9fe8f6f7660cfa3f7ee7cdb71cc8f8f
brianmina <brian.mina17@gmail.com>
2023-10-04 09:51:49 -0400

    while loop

--- chapter_7_input_while_loop/parrot.py
+++ chapter_7_input_while_loop/parrot.py
@@ -1,4 +1,22 @@
+#
+#
+# prompt = "\ntell me something, and I will repeat it back to you:"
+# prompt += "\nEnter 'quit' to end the program. "
+#
+# message = ""
+# while message != 'quit':
+#     message = input(prompt)
+#     if message != 'quit':
+#         print(message)
 
 
-message = input( "tell me something, and I will repeat it back to you: ")
-print(message)
+prompt = "\ntell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
