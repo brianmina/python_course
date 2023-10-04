ae44f5ad39edee88d96ca85e50c0ac5780dd6b53
brianmina <brian.mina17@gmail.com>
2023-10-04 15:47:13 -0400

    infitine loop

---
+++ chapter_7_input_while_loop/exercise_2.py
@@ -0,0 +1,53 @@
+#
+#
+# prompt = "\nAdd some toppings to your pizza: "
+# prompt += "\n(Enter 'quit' when you finished.)"
+#
+# while True:
+#     toppings = input(prompt)
+#     if toppings == 'quit':
+#         break
+#     else:
+#         print(f"Ok, adding {toppings} to your pizza!")
+#
+#
+# prompt = input("How old are you?: ")
+# age = int(prompt)
+# if age <= 3:
+#     print("Your ticket is for free!")
+# elif age <= 12:
+#     print("Your ticket is $10.")
+# else:
+#     print("Your ticket is $15.")
+#
+#
+# prompt = "\nwhat is your favorite number?: "
+# prompt += "\n(Enter 'quit' when you finished.)"
+#
+# number = ""
+#
+# active = True
+#
+# while True:
+#     number = input(prompt)
+#     if number == 'quit':
+#         break
+#     else:
+#         print("I like that number!")
+
+
+prompt = "\nfavorite color?: "
+prompt += "\n(Enter 'quit' when you finished.)"
+
+active = True
+
+while True:
+    color = input(prompt)
+    if color == 'quit':
+        break
+
+
+
+
+
+
